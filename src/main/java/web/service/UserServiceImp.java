545babef591bd2b94e0023a59ba35ae172ea19d7
OlI1551 <I15510000000@gmail.com>
2024-10-06 22:04:41 +0800

    fifth commit - EntityNotFoundException is added to UserDaoImp methods and @Transactional annotation moved to Service

--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -2,6 +2,7 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.model.User;
 import java.util.List;
@@ -19,11 +20,13 @@ public class UserServiceImp implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> getUsersList() {
         return  userDao.getUsersList();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> getUsersList(int count) {
         if (count >= userDao.getUsersList().size()) {
             return userDao.getUsersList();
@@ -33,21 +36,25 @@ public class UserServiceImp implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getUserById(long id) {
         return userDao.getUserById(id);
     }
 
     @Override
+    @Transactional
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
     @Override
+    @Transactional
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
     @Override
+    @Transactional
     public void deleteUserById(Long id) {
         userDao.deleteUserById(id);
     }
